854c65c580a9eeee8415a4e58c0d94c2f088fd65
xblade <39009260+xbladecod3r@users.noreply.github.com>
2018-10-26 06:26:09 -0700

    Add files via upload

---
+++ lang/en/lang_browse.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang = array(
+
+#Browse
+'browse_error' => "Error",
+'browse_invalid_cat' => "Invalid category ID.",
+'browse_search' => "Search results for ",
+'browse_show_all' => "Show all",
+'browse_active' => "active",
+'browse_inc_dead' => "including dead",
+'browse_dead' => "only dead",
+'browse_go' => "Go!",
+'browse_search' => "Search results for ",
+'browse_not_found' => "Nothing found!",
+'browse_tryagain' => "Try again with a refined search string.",
+'browse_nothing' => "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nothing here!",
+'browse_sorry' => "&nbsp;&nbsp;Sorry pal :",
+'browse_ddls' => "Downloads Page Find All You Need Here With Categories To Suit Every One",
+'browse_fancse' => "Find All You Need Here With Categories To Suit Every One",
+
+);
+
+?>
